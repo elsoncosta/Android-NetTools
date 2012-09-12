f0a28bd757fa76247819ced2a9d08a392103b44d
Pietro Caselani <pc1992@gmail.com>
2012-09-11 21:16:01 -0300

    Small adjustments

--- Example/src/com/pc/nettols/example/TasksActivity.java
+++ Example/src/com/pc/nettols/example/TasksActivity.java
@@ -2,17 +2,16 @@ package com.pc.nettols.example;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
-import com.pc.nettools.AsyncRequest;
+import com.pc.nettools.http.AsyncHttpRequest;
 import com.pc.nettools.http.JSONResponseHandler;
-import com.pc.nettools.tasks.HttpTask;
+
+import java.util.ArrayList;
 
 /**
  * Created by Pietro Caselani
  */
-public class TasksActivity extends Activity implements AsyncRequest.AsyncRequestListener {
+public class TasksActivity extends Activity{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,32 +19,26 @@ public class TasksActivity extends Activity implements AsyncRequest.AsyncRequest
         setContentView(R.layout.task_activity);
 
         findViewById(R.id.task_json).setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                HttpTask jsonTask = new HttpTask(JSONResponseHandler.class);
-                jsonTask.setRequestListener(TasksActivity.this);
-                jsonTask.start(Utils.JSON_TASK);
+                AsyncHttpRequest.request(Utils.JSON_TASK, new JSONResponseHandler() {
+                    @Override
+                    public void onSuccess(ArrayList json, AsyncHttpRequest request) {
+                        super.onSuccess(json, request);
+                    }
+
+                    @Override
+                    public void onFailure(Exception exception, AsyncHttpRequest request) {
+                        super.onFailure(exception, request);
+                    }
+
+                    @Override
+                    public void onProgress(int progress, AsyncHttpRequest request) {
+                        super.onProgress(progress, request);
+                    }
+                });
             }
         });
-    }
-
-    @Override
-    public void onSuccess(Object o, AsyncRequest request) {
-        ((TextView) findViewById(R.id.textView_task)).setText(o.toString());
-    }
-
-    @Override
-    public void onFailure(Exception exception, AsyncRequest request) {
-        ((TextView) findViewById(R.id.textView_task)).setText(exception.toString());
-    }
-
-    @Override
-    public void onProgressChanged(int progress, AsyncRequest request) {
-        Log.i("PROGRESS-TASK", String.valueOf(progress) + "%");
-    }
-
-    @Override
-    public void onCancelled(AsyncRequest request) {
-
     }
 }
